6fbe8043ea62bce57747565073035b38448b7ebe
au724764 <au724764@uni.au.dk>
2024-06-14 00:16:08 +0200

    Add how to video

--- src/views/HowTo.vue
+++ src/views/HowTo.vue
@@ -2,8 +2,8 @@
   <div class="text-center p-3">
     <iframe
       class="w-8 h-screen max-h-30rem"
-      src="https://www.youtube.com/embed/m2D-_AAj_4A?si=zSzb5-utplpuWc7w"
-      title="YouTube video player"
+      src="https://www.youtube.com/embed/m_FKCOg8p3o?si=HA96-3eq3uQNhYdq&hd=1"
+      title="Maskinrummet Intro - YouTube"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       referrerpolicy="strict-origin-when-cross-origin"
